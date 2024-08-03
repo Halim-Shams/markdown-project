fe5e00ef723e3f55737af7e401eeb21b568220a5
Halim <halimshams8@gmail.com>
2024-08-03 15:58:54 +0430

    ui finished

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Markdown from './Markdown';
+import Button from './Button';
 
 const Body = () => {
 	return (
 		<form className='border-x border-gray-200 min-h-screen w-[52rem] px-12 pt-14 flex flex-col gap-2'>
+			<Button text='Preview' type='submit' />
 			<textarea
 				className='text-4xl font-semibold outline-none text-gray-800 placeholder:text-gray-400 font-serif overflow-hidden resize-none px-2'
 				rows='2'
